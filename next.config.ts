a65020aa7b9accf77b9be49a101e220cd05c3907
kazuha2004 <147528700+kazuha2004@users.noreply.github.com>
2026-04-19 03:16:42 +0530

    fix: add skipGlobalErrorPrerenderingFallback

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    skipGlobalErrorPrerenderingFallback: true,
+  },
 };
 
 export default nextConfig;
