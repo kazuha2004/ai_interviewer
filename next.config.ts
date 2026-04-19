bc6b8786bbd2fd2ed0c26cec12839c899f1604b8
kazuha2004 <147528700+kazuha2004@users.noreply.github.com>
2026-04-19 09:46:42 +0530

    fix: remove not-found and global-error to fix prerender build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: 'standalone',
+  /* config options here */
 };
 
 export default nextConfig;
